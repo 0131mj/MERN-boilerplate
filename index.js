a1abf6eac9f67760bc7a7cc231d0551968d25e19
mjbae <0131mj@gmail.com>
2020-12-29 01:23:54 +0900

    몽고 DB 연결

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://bae:1234@boilerplate.72x4c.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error(err));
+
 app.get('/', (req, res) => res.send('Hello, world!'))
 
-app.listen(port, ()=> console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
