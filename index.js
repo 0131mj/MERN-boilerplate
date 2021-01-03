c967d9a1b389c7e1413c166e3c042d9f0b0870a9
mjbae <0131mj@gmail.com>
2021-01-03 22:56:36 +0900

    회원등록 코드 추가

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser")
+const { User } = require("./models/Users")
+
+// application /x-www-from-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/ 
+app.use.apply(bodyParser.json())
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://bae:1234@boilerplate.72x4c.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -12,5 +20,17 @@ mongoose.connect('mongodb+srv://bae:1234@boilerplate.72x4c.mongodb.net/<dbname>?
     .catch(err => console.error(err));
 
 app.get('/', (req, res) => res.send('Hello, world!'))
+
+app.post('/register', (req, res) => {
+    // 회원 가입시 필요한 정보들을 client 에서 가져오면
+    // 그것들을 데이터베이스에 넣어준다. 
+    const user = new User(req.body);
+    user.save((err, doc)=>{
+        if(err) return res.json( { success: false, err})
+        return res.status(200).json({
+            success: true,
+        })
+    }); // mongoDB 메소드
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
