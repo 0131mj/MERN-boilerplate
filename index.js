7387bfe15489f1ff2476d5ae246e66b3df4be23e
mjbae <0131mj@gmail.com>
2021-01-08 01:04:28 +0900

    auth 기능 추가

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
+const auth = require('./middleware/auth');
 
 const { User } = require("./models/Users")
 
@@ -26,7 +27,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello, world!'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     // 회원 가입시 필요한 정보들을 client 에서 가져오면
     // 그것들을 데이터베이스에 넣어준다. 
     const user = new User(req.body);
@@ -38,7 +39,7 @@ app.post('/register', (req, res) => {
     }); // mongoDB 메소드
 });
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
@@ -54,20 +55,29 @@ app.post('/login', (req, res) => {
 
             // 비밀번호까지 같다면 토큰을 생성하기.
             user.generateToken((err, user) => {
-                if(err) return res.status(400).send(err);
+                if (err) return res.status(400).send(err);
 
                 // 토큰을 저장한다. 쿠키에.
                 res.cookie("x_auth", user.token)
-                .status(200)
-                .json({loginSuccess: true, userId: user._id})
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id })
             })
         })
     })
+})
 
-
-
-
-
+app.get('/api/users/auth', auth, (req, res) => {
+    // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 true 라는 말
+    req.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
