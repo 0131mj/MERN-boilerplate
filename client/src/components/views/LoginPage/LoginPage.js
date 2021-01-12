85b14ad4a72c7b00b7071d0fe4d0b0e67f70c2ca
mjbae <0131mj@gmail.com>
2021-01-13 01:07:15 +0900

    axios 추가

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios';
 
 function LoginPage() {
+    useEffect(() => {    
+        axios.get('http://localhost:5000/api/hello').then(response => console.log(response.data));
+    }, [])
     return (
         <div>
             LoginPage
--- server/index.js
+++ server/index.js
@@ -27,6 +27,10 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello, world!'))
 
+app.get('/api/hello', (req, res) => {
+    res.send("안녕하세요.")
+})
+
 app.post('/api/users/register', (req, res) => {
     // 회원 가입시 필요한 정보들을 client 에서 가져오면
     // 그것들을 데이터베이스에 넣어준다. 
