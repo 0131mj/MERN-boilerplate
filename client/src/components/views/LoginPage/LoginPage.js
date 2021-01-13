20cbd73692410252ae33762cea7d01ec7486eb0e
mjbae <0131mj@gmail.com>
2021-01-14 00:05:52 +0900

    Proxy 설정

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,10 +1,7 @@
-import React, { useEffect } from 'react'
-import axios from 'axios';
+import React from 'react'
 
 function LoginPage() {
-    useEffect(() => {    
-        axios.get('http://localhost:5000/api/hello').then(response => console.log(response.data));
-    }, [])
+    
     return (
         <div>
             LoginPage
