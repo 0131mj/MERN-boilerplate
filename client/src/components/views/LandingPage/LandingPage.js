b0fe13d9fb498a4f44bd5bc346438bb251f03cc4
mjbae <0131mj@gmail.com>
2021-01-22 01:01:34 +0900

    로그아웃 처리

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,17 +1,32 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-function LandingPage() {
+
+
+function LandingPage(props) {
     useEffect(() => {
         axios.get('/api/hello').then(response => console.log(response));
-    }, [])
+    }, []);
+
+    const onClickHandler = () => {
+        axios.get(`/api/users/logout`)
+        .then(response => {
+            if(response.data.success){
+                props.history.push("/login")
+            }else{
+                alert("로그아웃에 실패했습니다.")
+            }
+        })
+    }
+    
     return (
         <div style={{
-            display: "flex", justifyContent: "center", alignItems: "center",
+            display: "flex", justifyContent: "center", alignItems: "center", flexDirection:"column",
             width: "100vw", height: "100vh"
         }}>
             <h2>시작 페이지</h2>
-        </div>
+            <button onClick={onClickHandler}>로그아웃</button>
+        </div>        
     )
 }
 
